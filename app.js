ed1e328db5c4e19cf12309a1b687e2917a55aad7
palani <palanik987@gmail.com>
2018-07-16 12:51:38 +0530

    os specific paths

--- app.js
+++ app.js
@@ -3,87 +3,84 @@ const fs = require('fs');
 const args = require('yargs').argv;
 const replace = require('replace');
 
-const getJs = (files,dirName,chunks = 10) => {
+const getJs = (files, dirName, chunks = 10) => {
     var dir = {
-        path: dirName,
+        path: path.resolve(dirName),
         chunks: [],
-        compiled_js: {
-            'vendor': '',
-            'inline': '',
-            'main': '',
-            'polyfills': ''
+        compiled_files: {
+            'inline':'',
+            'vendor':'',
+            'main':'',
+            'polyfills':'',
+            'styles':''
         },
-        style:'',
-        valid:true,
-        error:'No Errors'
+        valid: true,
+        error: ''
     };
     files.forEach((file) => {
         if (path.parse(file).ext == '.js') {
             if (!isNaN(parseInt(file.split('.')[0]))) {
                 dir.chunks.push(file);
             } else {
-                dir.compiled_js[file.split('.')[0]] = file;
+                dir.compiled_files[file.split('.')[0]] = file;
             }
-        }else if(path.parse(file).ext =='.css') {
-            dir.style = file;
+        } else if (path.parse(file).ext == '.css') {
+            dir.compiled_files[file.split('.')[0]] = file;
         }
     });
-    if (dir.chunks.length == chunks){
-        for (var js in dir.compiled_js){
-            if(dir.compiled_js[js].length == 0){
+    if (dir.chunks.length == chunks) {
+        for (var js in dir.compiled_files) {
+            if (dir.compiled_files[js].length == 0) {
                 dir.valid = false;
-                dir.error = 'No of compiled files is incorrect';
+                dir.error = 'incorrect number of compiled files';
             }
         }
-    }else{
+    } else {
         dir.valid = false;
-        dir.error = 'Unmet chunks count';
+        dir.error = `invalid number of chunk files - required(${chunks})`;
     }
-    dir.valid ? console.log(`${dirName} is valid`):console.log(`${dirName} is invalid`);
     return dir;
 };
 
-var source = getJs(fs.readdirSync(args.src),args.src,args.chunks);
-var destination = getJs(fs.readdirSync(args.dest),args.dest,args.chunks);
+try {
+    var source = getJs(fs.readdirSync(args.src), args.src, args.chunks);
+    var destination = getJs(fs.readdirSync(args.dest), args.dest, args.chunks);
+    if (source.valid && destination.valid) {
+        for (var i = 0; i < destination.chunks.length; i++) {
+            fs.unlinkSync(path.join(destination.path, destination.chunks[i]));
+            fs.createReadStream(path.join(source.path, source.chunks[i])).pipe(fs.createWriteStream(path.join(destination.path, source.chunks[i])));
+        };
 
-if((source.valid && destination.valid)&&(source.chunks.length == destination.chunks.length)){
+        for (var type in destination.compiled_files) {
+            fs.unlinkSync(path.join(destination.path, destination.compiled_files[type]));
+            fs.createReadStream(path.join(source.path, source.compiled_files[type])).pipe(fs.createWriteStream(path.join(destination.path, source.compiled_files[type])));
+        }
 
-    for (var i = 0;i<destination.chunks.length;i++){
-        fs.unlinkSync(destination.path+'\\'+destination.chunks[i]);
-        fs.createReadStream(source.path+'\\'+source.chunks[i]).pipe(fs.createWriteStream(destination.path+'\\'+source.chunks[i]));
-    };
+        for (var type in destination.compiled_files) {
+            replace({
+                regex: destination.compiled_files[type],
+                replacement: source.compiled_files[type],
+                paths: [path.join(destination.path, 'templates', 'production-code.twig')],
+                recursive: true,
+                silent: true
+            });
+        }
 
-    for (var type in destination.compiled_js){
-        fs.unlinkSync(destination.path+'\\'+destination.compiled_js[type]);
-        fs.createReadStream(source.path+'\\'+source.compiled_js[type]).pipe(fs.createWriteStream(destination.path+'\\'+source.compiled_js[type]));
+        if (args.index == 'true') {
+            fs.unlinkSync(path.join(destination.path, 'index.html'));
+            fs.createReadStream(path.join(source.path, 'index.html')).pipe(fs.createWriteStream(path.join(destination.path, 'index.html')));
+        }
+
+        console.log('Build files are transferred succesfully!!!');
+
+    } else {
+        var dirs = [source, destination];
+        for (var dir in dirs) {
+            if (!dirs[dir].valid) {
+                console.log(`${dirs[dir].path} directory has ${dirs[dir].error}`);
+            }
+        }
     }
-
-    fs.unlinkSync(destination.path+'\\'+destination.style);
-    fs.createReadStream(source.path+'\\'+source.style).pipe(fs.createWriteStream(destination.path+'\\'+source.style));
-    
-    replace({
-        regex: destination.style,
-        replacement: source.style,
-        paths: [destination.path+'\\templates\\production-code.twig'],
-        recursive: true,
-        silent: true
-    });
-
-    for (var type in destination.compiled_js){
-        replace({
-            regex: destination.compiled_js[type],
-            replacement: source.compiled_js[type],
-            paths: [destination.path+'\\templates\\production-code.twig'],
-            recursive: true,
-            silent: true
-        });
-    }
-
-    if(args.index =='true'){
-        fs.unlinkSync(destination.path+'\\index.html');
-        fs.createReadStream(source.path+'\\index.html').pipe(fs.createWriteStream(destination.path+'\\index.html'));
-    }
-
-}else{
-    console.log(`Directory path is invalid or it doesn't met the requirements`);
+}catch(err){
+    console.log(`please provide correct directory`);
 }
