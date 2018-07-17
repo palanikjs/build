57575dff02ddf89673d3206ef8bd7655026fd2bb
palani <palanik987@gmail.com>
2018-07-17 18:38:35 +0530

    yargs help

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
 const path = require('path');
 const fs = require('fs');
-const args = require('yargs').argv;
 const readYaml = require('read-yaml');
 const writeYaml = require('write-yaml');
 const replace = require('replace');
+const args = require('yargs')
+    .usage('node $0 [args]')
+    .option('source',{
+        alias:['src','s'],
+        describe:'Folder which contains the compiled files. like /spa/dist',
+        demand:true,
+        demand:'Source directory is required see --help for Usage'
+    })
+    .option('destination',{
+        alias:['dest','d'],
+        describe:'Directory to which the compiled files to be moved.',
+        demand:true,
+        demand:'Destination directory is required see --help for Usage'
+    })
+    .option('chunks',{
+        alias:'c',
+        describe:'Number of chunk files to be validated',
+        default:10
+    })
+    .option('index',{
+        alias:'i',
+        describe:'Changing the contents of index.html file',
+        default:false
+    })
+    .option('type',{
+        alias:['t','v'],
+        describe:'For version update in *.info.yml file',
+        default:'patch'
+    })
+    .help('help')
+    .alias('h','help')
+    .argv;
 
 const getJs = (files, dirName, chunks = 10) => {
     var dir = {
@@ -45,19 +76,24 @@ const getJs = (files, dirName, chunks = 10) => {
 };
 
 try {
+    
     var source = getJs(fs.readdirSync(args.src), args.src, args.chunks);
     var destination = getJs(fs.readdirSync(args.dest), args.dest, args.chunks);
+
     if (source.valid && destination.valid) {
-        for (var i = 0; i < destination.chunks.length; i++) {
+        console.log(`${destination.chunks.length} - chunks found!\nTransferring files from [ ${source.path} ] to [ ${destination.path} ]`);
+        
+        //copying chunk files.
+        for (var i = 0; i < parseInt(args.chunks); i++) {
             fs.unlinkSync(path.join(destination.path, destination.chunks[i]));
             fs.createReadStream(path.join(source.path, source.chunks[i])).pipe(fs.createWriteStream(path.join(destination.path, source.chunks[i])));
         };
-
+        //copying compiled files.
         for (var type in destination.compiled_files) {
             fs.unlinkSync(path.join(destination.path, destination.compiled_files[type]));
             fs.createReadStream(path.join(source.path, source.compiled_files[type])).pipe(fs.createWriteStream(path.join(destination.path, source.compiled_files[type])));
         }
-
+        //replacing the file names in production-code.twig.
         for (var type in destination.compiled_files) {
             replace({
                 regex: destination.compiled_files[type],
@@ -67,13 +103,13 @@ try {
                 silent: true
             });
         }
-
+        //replacing the index.html file incase of any addition or deletion of cdn files.
         if (args.index == 'true') {
             fs.unlinkSync(path.join(destination.path, 'index.html'));
             fs.createReadStream(path.join(source.path, 'index.html')).pipe(fs.createWriteStream(path.join(destination.path, 'index.html')));
         }
 
-        console.log('Build files are transferred succesfully!!!');
+        console.log('Files transferred succesfully!!!');
 
         try {
             var yaml = readYaml.sync(path.join(destination.path, 'sandbox.info.yml'));
@@ -92,7 +128,7 @@ try {
             }
             yaml.version = version.join('.');
             writeYaml.sync(path.join(destination.path,'sandbox.info.yml'),yaml);
-            console.log(`version updated to ${yaml.version}`);
+            console.log(`Type - ${type} update done and version updated to ${yaml.version}`);
         } catch (err) {
             console.log('Invalid sandbox.info.yml file found');
         }
@@ -106,5 +142,5 @@ try {
         }
     }
 } catch (err) {
-    console.log(`please provide correct directory`);
+    console.log(`please provide correct directory path for source and destination!`);
 }
